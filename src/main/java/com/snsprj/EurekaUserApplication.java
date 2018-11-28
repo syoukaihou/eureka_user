716500df0f57a224f39cba181110832c0630d740
syoukaihou@gmail.com <syoukaihou@gmail.com>
2018-11-28 10:38:57 +0800

    项目初始化

---
+++ src/main/java/com/snsprj/EurekaUserApplication.java
@@ -0,0 +1,12 @@
+package com.snsprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaUserApplication.class, args);
+    }
+}
